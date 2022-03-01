da6a3249a6a4cb6c29caf0d2635472dc5b601748
naciyeceren097 <naciye.ceren097@gmail.com>
2022-03-02 01:04:43 +0300

    Added ordering processes.

--- client/src/pages/Basket/index.js
+++ client/src/pages/Basket/index.js
@@ -1,10 +1,51 @@
-import React from 'react';
+import React, {useRef,useState} from 'react';
 import {Link} from "react-router-dom";
 import {useBasket} from '../../contexts/BasketContext';
-import {Alert, Button, Image,Box,Grid,Text} from "@chakra-ui/react";
+import {Alert, Button, Image,Box,Grid,Text,Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+    useDisclosure,
+FormControl,
+FormLabel,
+Input, 
+Textarea,
+useToast
+} from "@chakra-ui/react";
+import { postOrder } from '../../api';
 
 function Basket() {
-    const {items, removeFromBasket} = useBasket();
+    const {items, removeFromBasket, emptyBasket} = useBasket();
+    const { isOpen, onOpen, onClose } = useDisclosure();
+
+    //toast
+    const toast = useToast()
+
+    //modal
+    const [address, setAddress] = useState('');
+    const initialRef = React.useRef();
+    const handleSubmitForm = async () => {
+        const itemIds = items.map((item) => item._id);
+
+        const input = {
+            address,
+            items: JSON.stringify(itemIds)
+        }
+        const response = await postOrder(input);
+        emptyBasket();
+        onClose();
+        toast({
+            title: 'Your order has been received successfully.',
+            status: 'success',
+            position: 'top-right',
+            duration: 9000,
+            isClosable: true,
+          })
+        console.log(response);
+    }
 
     const total = items.reduce((acc, obj) => acc + obj.price, 0);
   return (
@@ -35,6 +76,32 @@ function Basket() {
                     <Box mt={10}>
                          <Button fontSize="22">Total: {total}</Button>
                     </Box>
+                    <Button mt={2} size="lg" colorScheme="orange" onClick={onOpen}>Order</Button>
+                    <Modal initialFocusRef={initialRef} isOpen={isOpen} onClose={onClose}>
+                        <ModalOverlay />
+                        <ModalContent>
+                            <ModalHeader>Order</ModalHeader>
+                            <ModalCloseButton />
+                            <ModalBody pb={6}>
+                                <FormControl>
+                                    <FormLabel>Address</FormLabel>
+                                    <Textarea 
+                                        ref={initialRef} 
+                                        placeholder='Address'
+                                        value={address}
+                                        onChange={(e) => setAddress(e.target.value)}
+                                        />
+                                </FormControl>
+                            </ModalBody>
+
+                            <ModalFooter>
+                                <Button colorScheme='blue' mr={3} onClick={handleSubmitForm}>
+                                 Save
+                                </Button>
+                                <Button onClick={onClose}>Cancel</Button>
+                            </ModalFooter>
+                        </ModalContent>
+                    </Modal>
           </div>
       }
     </div>
